ca386128c020bce4bffdef0ee6460a16ac7327a2
jesusadvanzer <jesus.salas@advanzer.com>
2015-07-20 17:02:39 -0500

    pagina de inicio

--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,25 +1,19 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
   <div class="container">
-    <h1>Bienvenido al sitio de Capital Humano</h1>
-    <p>Este sitio fue creado para compartir contigo información relevante de Capital Humano. Se encuentra en actualización permanente por lo que te invitamos a visitarlo y explorarlo para mantenerte al tanto de los cambios.</p>
-    <p>Nos ponemos a tus órdenes para todo tema relacionado con el área.</p>
+    <h1>Bienvenido</h1>
+    <p>Aquí encontrarás los objetivos y las competencias que tu(s) evaluador(es) utilizarán para 
+      medir tu desempeño durante 2014.</p>
+    <p>Además de otros servicios dependiendo de tu perfil.</p>
   </div>
 </div>
 <div class="container">
   <div class="row">
-  <div class="col-md-4" align="center">
-    <h3><b>&nbsp;</b></h3>
+    <div class="col-md-6" align="center">
+      <h3><b>&nbsp;</b></h3>
       <p><img src="<?= base_url('assets/images/recursos-humanos.jpg'); ?>"></p>
     </div>
-    <div class="col-md-4" align="center">
-      <h3><b>Contáctanos:</b></h3>
-      <p>&nbsp;</p> 
-      <p><a href="mailto:micaela.llano@advanzer.com">micaela.llano@advanzer.com</a><br>
-      <a href="mailto:enrique.bernal@advanzer.com">enrique.bernal@advanzer.com</a></p>
-      <p>Tel. 8286-9690 ext. 9005<br>Directo: 1769-9069</p>
-    </div>
-    <div class="col-md-4" align="center">
+    <div class="col-md-6" align="center">
       <h3>&nbsp;</h3>
       <p><img style="max-height:200px" src="<?= base_url('assets/images/Vision2020.jpg');?>"></p>
     </div>
