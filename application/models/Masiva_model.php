14c6de27e2dbc313efb5a5372218aac31c6a2628
Jesus <jesus.salas@advanzer.com>
2015-10-21 16:53:53 -0500

    se adecúa formatoo para carga inicial

--- application/models/Masiva_model.php
+++ application/models/Masiva_model.php
@@ -11,18 +11,18 @@ class Masiva_model extends CI_Model{
 	function dropTables($tipo) {
 		switch ($tipo) {
 			case 1:
-				if($this->db->truncate('Porcentajes_Objetivos'))
-					if($this->db->truncate('Metricas'))
-						if($this->db->truncate('Objetivos_Areas'))
-							if($this->db->truncate('Objetivos'))
-								if($this->db->truncate('Dominios'))
+				if($this->db->empty_table('Porcentajes_Objetivos'))
+					if($this->db->empty_table('Metricas'))
+						if($this->db->empty_table('Objetivos_Areas'))
+							if($this->db->empty_table('Objetivos'))
+								if($this->db->empty_table('Dominios'))
 									return true;
 				break;
 			case 2:
-				if($this->db->truncate('Comportamiento_Posicion'))
-					if($this->db->truncate('Comportamientos'))
-						if($this->db->truncate('Competencias'))
-							if($this->db->truncate('Indicadores'))
+				if($this->db->empty_table('Comportamiento_Posicion'))
+					if($this->db->empty_table('Comportamientos'))
+						if($this->db->empty_table('Competencias'))
+							if($this->db->empty_table('Indicadores'))
 								return true;
 				break;
 		}
@@ -41,11 +41,9 @@ class Masiva_model extends CI_Model{
 							$this->db->insert('Indicadores',array('nombre'=>$value));
 							if($this->db->affected_rows() == 1)
 								$indicador=$this->db->insert_id();
-							else
-								return false;
 						}
 						break;
-					case 'C':
+					case 'B':
 						$result=$this->db->select('id')->where('nombre',$value)->get('Competencias');
 						if($result->num_rows() == 1)
 							$competencia=$result->first_row()->id;
@@ -53,37 +51,29 @@ class Masiva_model extends CI_Model{
 							$this->db->insert('Competencias',array('nombre'=>$value,'indicador'=>$indicador));
 							if($this->db->affected_rows() == 1)
 								$competencia=$this->db->insert_id();
-							else
-								return false;
 						}
+						break;
+					case 'C':
+						$result=$this->db->select('descripcion')->where('id',$competencia)->get('Competencias')->first_row()->descripcion;
+						if(!$result)
+							$this->db->where('id',$competencia)->update('Competencias',array('descripcion'=>$value));
 						break;
 					case 'D':
-						$result=$this->db->select('descripcion')->where('id',$competencia)->get('Competencias')->first_row()->descripcion;
-						if(!$result){
-							$this->db->where('id',$competencia)->update('Competencias',array('descripcion'=>$value));
-							if($this->db->affected_rows() != 1)
-								return false;
-						}
-						break;
-					case 'E':
 						$comportamiento=explode('.', $value);
-						$this->db->insert('Comportamientos',array('descripcion'=>$comportamiento[1],
+						$this->db->insert('Comportamientos',array('descripcion'=>trim($comportamiento[1]),
 							'competencia'=>$competencia));
 						if($this->db->affected_rows() == 1)
 							$comportamiento=$this->db->insert_id();
 						break;
+					case 'E':
+					case 'F':
 					case 'G':
 					case 'H':
 					case 'I':
 					case 'J':
-					case 'K':
-					case 'L':
 						if($value == 'ü')
-							$value=1;
-						else
-							$value=0;
-						$this->db->insert('Comportamiento_Posicion',array('nivel_posicion'=>$head[$row],
-							'comportamiento'=>$comportamiento,'evalua'=>$value));
+							$this->db->insert('Comportamiento_Posicion',array('nivel_posicion'=>$head[$row],
+								'comportamiento'=>$comportamiento));
 						break;
 					default:
 						# code...
@@ -99,10 +89,8 @@ class Masiva_model extends CI_Model{
 		foreach ($rows as $key => $val) {
 			if(count($val) >= 30)
 				foreach ($val as $row => $value) :
-					if($row == "A" && $row=="")
-						break;
 					switch ($row) {
-						case 'A':
+						case 'B':
 							$result=$this->db->select('id')->where('nombre',$value)->get('Dominios');
 							if($result->num_rows() == 1)
 								$dominio=$result->first_row()->id;
@@ -110,24 +98,17 @@ class Masiva_model extends CI_Model{
 								$this->db->insert('Dominios',array('nombre'=>$value));
 								if($this->db->affected_rows() == 1)
 									$dominio=$this->db->insert_id();
-								else
-									return false;
 							}
 							break;
-						case 'B':
+						case 'C':
 							$this->db->insert('Objetivos',array('nombre'=>$value,'dominio'=>$dominio));
 							if($this->db->affected_rows() == 1)
 								$objetivo=$this->db->insert_id();
-							else
-								return false;
-							break;
-						case 'C':
-							$this->db->where('id',$objetivo)
-								->update('Objetivos',array('descripcion'=>$value));
-							if($this->db->affected_rows() != 1)
-								return false;
 							break;
 						case 'D':
+							$this->db->where('id',$objetivo)->update('Objetivos',array('descripcion'=>$value));
+							break;
+						case 'E':
 							$metricas=explode("\n",$value);
 							$j=0;
 							for ($i=5; $i > 0; $i--) :
@@ -138,26 +119,21 @@ class Masiva_model extends CI_Model{
 									return false;
 							endfor;
 							break;
-						case 'X':
 						case 'Y':
 						case 'Z':
 						case 'AA':
 						case 'AB':
 						case 'AC':
+						case 'AD':
 							$valor=$value*100;
 							$this->db->insert('Porcentajes_Objetivos',array('objetivo'=>$objetivo,'valor'=>$valor,
 								'nivel_posicion'=>$head[$row]));
 							if($this->db->affected_rows() != 1)
 								exit();
 							break;
-						case 'AD':
-							break;
 						default:
-							if($value=='SÍ'){
+							if($value=='SI')
 								$this->db->insert('Objetivos_Areas',array('objetivo'=>$objetivo,'area'=>$head[$row]));
-								if($this->db->affected_rows() != 1)
-									return false;
-							}
 							break;
 					}
 				endforeach;
