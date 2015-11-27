6b43218b2c397a78f338466f3ec1e723e0b03df4
jesusadvanzer <jesus.salas@advanzer.com>
2015-11-27 15:46:18 -0600

    bug tabla

--- application/views/evaluacion/revisar.php
+++ application/views/evaluacion/revisar.php
@@ -119,7 +119,7 @@
 	<div class="row">
 		<div class="col-md-12">
 			<h3><b>Evaluación:</b></h3>
-			<table id="tbl" align="center" class="display">
+			<table id="tbl" align="center" class="table display">
 				<thead>
 					<tr>
 						<th data-halign="center"></th>
