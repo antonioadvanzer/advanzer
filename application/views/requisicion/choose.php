0b3e174720b5f4c0b9d44d0233218f72a9280c41
Antonio Baez <antonio.baez@advanzer.com>
2016-05-24 11:32:56 -0500

    cambios esteticos

--- application/views/requisicion/choose.php
+++ application/views/requisicion/choose.php
@@ -10,7 +10,7 @@
 		<div class="col-md-6" style="padding-top:5%">
 			<p><label style="cursor:pointer;" onclick="location.href='<?= base_url("requisicion/nueva");?>';">
 				<span class="glyphicon glyphicon-chevron-right"></span>
-				Proyecto Advanzer
+				Proyecto Advanzer/Entuizer
 				<span class="glyphicon glyphicon-chevron-left"></span>
 			</label></p>
 		</div>
